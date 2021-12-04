d0a1965d71361a15492d2bbe375e80e80087ffd7
Saimic <raishahidrai@gmail.com>
2021-12-04 23:11:36 +0500

    Add files via upload

---
+++ reporting/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import include,path
+from . import views
+
+
+urlpatterns =[
+    
+   
+    #path('reporting',views.reporting,name='reportingModule'),
+    #path('edd',views.edd,name='eddModule'),
+    
+    
+    ]
---
+++ reporting/views.py
@@ -0,0 +1,17 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpRequest
+
+import sys
+
+
+
+
+
+
+#def reporting(request):
+#      return render(request,"reporting/report_module.html")
+      
+    #individual = Individual.objects.all()
+    #return render(request,"reporting/report_module.html",{'individual': individual})
+
